6249bda6c0c59fbc92b9818f7c39e9364866b0c4
sadohu <sadohu26@yahoo.com>
2023-08-09 02:38:53 -0500

    Implementacion Tarjeta

---
+++ C5_PJ_Restaurante_API/Controllers/TarjetaController.cs
@@ -0,0 +1,43 @@
+﻿using C5_PJ_Restaurante_API.Business;
+using C5_PJ_Restaurante_API.Models;
+using C5_PJ_Restaurante_API.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C5_PJ_Restaurante_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarjetaController : Controller
+    {
+        private ITarjeta iTarjeta;
+
+        public TarjetaController()
+        {
+            iTarjeta = new TarjetaRepository();
+        }
+
+        [HttpGet("getTarjeta")]
+        public async Task<ActionResult<IEnumerable<tb_tarjeta>>> Listar(int id_usuario)
+        {
+            return Ok(await Task.Run(() => iTarjeta.Get(id_usuario)));
+        }
+
+        [HttpPost("saveTarjeta")]
+        public async Task<ActionResult<string>> Agregar(tb_tarjeta direccion)
+        {
+            return Ok(await Task.Run(() => iTarjeta.Add(direccion)));
+        }
+
+        [HttpPut("updateTarjeta")]
+        public async Task<ActionResult<string>> actualizarProducto(tb_tarjeta direccion)
+        {
+            return Ok(await Task.Run(() => iTarjeta.Update(direccion)));
+        }
+
+        [HttpPut("deleteTarjeta")]
+        public async Task<ActionResult<string>> eliminarProducto(int id_direccion)
+        {
+            return Ok(await Task.Run(() => iTarjeta.Delete(id_direccion)));
+        }
+    }
+}
---
+++ C5_PJ_Restaurante_API/Repository/TarjetaRepository.cs
@@ -0,0 +1,140 @@
+﻿using C5_PJ_Restaurante_API.Business;
+using C5_PJ_Restaurante_API.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace C5_PJ_Restaurante_API.Repository
+{
+    public class TarjetaRepository : ITarjeta
+    {
+        private string connectionString;
+
+        public TarjetaRepository()
+        {
+            connectionString = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetConnectionString("connection");
+        }
+
+        public IEnumerable<tb_tarjeta> Get(int id)
+        {
+            List<tb_tarjeta> lista = new();
+            using (SqlConnection cn = new(connectionString))
+            {
+                SqlCommand cmd = new("SP_GETDIRECCION", cn)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                cmd.Parameters.AddWithValue("@ID_USUARIO", id);
+                cn.Open();
+                var dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    lista.Add(new tb_tarjeta()
+                    {
+                        id_tarjeta = dr.GetInt32(0),
+                        id_usuario = dr.GetInt32(1),
+                        numero_tarjeta = dr.GetString(2),
+                        cvv_tarjeta = dr.GetString(3),
+                        fecha_tarjeta = dr.GetString(4),
+                    });
+                }
+                cn.Close();
+            }
+            return lista;
+        }
+
+        public string Add(tb_tarjeta tarjeta)
+        {
+            string response = "";
+            using (SqlConnection cn = new(connectionString))
+            {
+                try
+                {
+                    SqlCommand cmd = new("SP_INSERTTARJETA", cn)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+                    cmd.Parameters.AddWithValue("@ID_USUARIO", tarjeta.id_usuario);
+                    cmd.Parameters.AddWithValue("@NUMERO", tarjeta.numero_tarjeta);
+                    cmd.Parameters.AddWithValue("@CVV", tarjeta.cvv_tarjeta);
+                    cmd.Parameters.AddWithValue("@FECHA", tarjeta.fecha_tarjeta);
+                    cmd.Parameters.AddWithValue("@NOMBRE", tarjeta.nombre_tarjeta);
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    response = "Se registró la tarjeta exitosamente.";
+                }
+                catch (Exception ex)
+                {
+                    response = ex.Message;
+                    cn.Close();
+                }
+                finally
+                {
+                    cn.Close();
+                }
+            }
+            return response;
+        }
+
+        public string Update(tb_tarjeta tarjeta)
+        {
+            string response = "";
+            using (SqlConnection cn = new(connectionString))
+            {
+                try
+                {
+                    SqlCommand cmd = new("SP_UPDATEDIRECCION", cn)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+                    cmd.Parameters.AddWithValue("@ID_TARJETA", tarjeta.id_tarjeta);
+                    cmd.Parameters.AddWithValue("@NUMERO", tarjeta.numero_tarjeta);
+                    cmd.Parameters.AddWithValue("@CVV", tarjeta.cvv_tarjeta);
+                    cmd.Parameters.AddWithValue("@FECHA", tarjeta.fecha_tarjeta);
+                    cmd.Parameters.AddWithValue("@NOMBRE", tarjeta.nombre_tarjeta);
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    response = "Se actualizó la tarjeta exitosamente.";
+                }
+                catch (Exception ex)
+                {
+                    response = ex.Message;
+                    cn.Close();
+                }
+                finally
+                {
+                    cn.Close();
+                }
+            }
+            return response;
+        }
+
+        public string Delete(int id)
+        {
+            string response = "";
+            using (SqlConnection cn = new(connectionString))
+            {
+                try
+                {
+                    SqlCommand cmd = new("SP_DELETETARJETA", cn)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+                    cmd.Parameters.AddWithValue("@ID_TARJETA", id);
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                    response = "Se eliminó la tarjeta exitosamente.";
+                }
+                catch (Exception ex)
+                {
+                    response = ex.Message;
+                    cn.Close();
+                }
+                finally
+                {
+                    cn.Close();
+                }
+            }
+            return response;
+        }
+    }
+}
